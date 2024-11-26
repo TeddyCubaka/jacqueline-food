e8e2e427bf67cccd8cc9051457596b5d06b21529
Teddy Cubaka <teddycubaka96@gmail.com>
2024-11-26 10:39:40 +0100

    add some juice images

--- src/components/sections/product.tsx
+++ src/components/sections/product.tsx
@@ -1,35 +1,60 @@
+import Image from "next/image";
+import mangue33cl from "@/../public/images/jus/mangue.jpg";
+import tanagawizi33cl from "@/../public/images/jus/tangawizi-33cl.jpg";
+import ananas33cl from "@/../public/images/jus/ananas-33cl.jpg";
+
 type ProductType = {
   id: number;
   name: string;
   price: string;
   description: string;
+  litrage: string;
+  url: string;
 };
 
-const products = [
+const products: ProductType[] = [
   {
     id: 1,
     name: "Jus de tangawizi",
     price: "1.7 $",
     description: "Tangawizi, citron et menthe",
+    litrage: "33cl",
+    url: tanagawizi33cl.src,
   },
   {
     id: 2,
-    name: "Jus Vert Détox",
-    price: "6.99€",
+    name: "Jus de mange",
+    price: "1.7 $",
     description: "Épinards, céleri, pomme et gingembre",
+    litrage: "33cl",
+    url: mangue33cl.src,
   },
   {
     id: 3,
-    name: "Smoothie Fruits Rouges",
-    price: "6.49€",
-    description: "Mélange de fruits rouges antioxydants",
+    name: "Jus d'ananas",
+    price: "1.7 $",
+    description: "100% Ananas",
+    litrage: "33cl",
+    url: ananas33cl.src,
   },
 ];
 
 const ProductCard = (product: ProductType) => {
   return (
     <div className="bg-white rounded-xl shadow-lg p-6 hover:shadow-xl transition duration-300">
-      <div className="h-48 bg-green-100 rounded-lg mb-4"></div>
+      <div className="h-60 bg-green-100 rounded-lg mb-4 overflow-hidden">
+        {product.url && product.url.length > 0 ? (
+          <Image
+            height={200}
+            width={200}
+            src={product.url}
+            alt={"image d'un jus"}
+            className="w-full h-auto"
+          />
+        ) : (
+          false
+        )}
+      </div>
       <h3 className="text-xl font-semibold text-gray-800 mb-2">
         {product.name}
       </h3>
