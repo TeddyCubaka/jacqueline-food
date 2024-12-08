733e5b0cc3415d6b4b2df4495dd387f44a149b6e
Teddy Cubaka <teddycubaka96@gmail.com>
2024-12-08 17:46:00 +0100

    make login endpoint compatible woth prisma

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,62 +1,53 @@
 import { NextResponse } from "next/server";
-import fs from "fs";
-import path from "path";
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken"; // Import JWT
+import jwt from "jsonwebtoken";
+import { prisma } from "@/../lib/prisma";
 
-const dbPath = path.join(process.cwd(), "src", "db.json");
-
-const SECRET_KEY = "votre_clé_secrète"; // Utilisez une clé secrète sécurisée ou mettez-la dans les variables d'environnement
+const SECRET_KEY =
+  process.env.JWT_SECRET_KEY ||
+  "jhfyfufyguyfyufutt ff yu yuyuyfguygyugiuhgoiuoinijho";
 
 type LoginBody = {
   login: string;
   password: string;
 };
 
-type User = {
-  id: number;
-  name: string;
-  password: string;
-  mail: string;
-};
-
 const missedField = (field: string) => {
   return NextResponse.json({
     code: 400,
-    message: `le champ ${field} est obligatoire`,
+    message: `Le champ ${field} est obligatoire`,
   });
 };
 
 export async function POST(request: Request) {
   try {
-    const dbData = JSON.parse(fs.readFileSync(dbPath, "utf8"));
-
     const reqBody: LoginBody = await request.json();
 
-    if (!("password" in reqBody)) return missedField("password");
-    if (!("login" in reqBody)) return missedField("login");
+    if (!reqBody.password) return missedField("password");
+    if (!reqBody.login) return missedField("login");
 
-    const dbUser: User | null = dbData.users.find(
-      (user: any) => user.mail === reqBody.login || user.name === reqBody.login
-    );
+    const dbUser = await prisma.user.findFirst({
+      where: {
+        OR: [{ mail: reqBody.login }, { name: reqBody.login }],
+      },
+    });
 
-    if (dbUser == null)
+    if (!dbUser) {
       return NextResponse.json({
         code: 404,
-        message: "mot de passe, mail ou nom d'utilisateur incorrect",
+        message: "Mot de passe, mail ou nom d'utilisateur incorrect",
       });
-
+    }
     const passwordMatch = await bcrypt.compare(
       reqBody.password,
       dbUser.password
     );
-    if (!passwordMatch)
+    if (!passwordMatch) {
       return NextResponse.json({
         code: 404,
-        message: "mot de passe, mail ou nom d'utilisateur incorrect",
+        message: "Mot de passe, mail ou nom d'utilisateur incorrect",
       });
-
-    // Génération du token
+    }
     const token = jwt.sign(
       {
         id: dbUser.id,
@@ -64,7 +55,7 @@ export async function POST(request: Request) {
         mail: dbUser.mail,
       },
       SECRET_KEY,
-      { expiresIn: "1h" } // Le token expirera dans 1 heure
+      { expiresIn: "1h" }
     );
 
     const user = {
@@ -74,9 +65,9 @@ export async function POST(request: Request) {
     };
 
     return NextResponse.json({
-      message: `bienvenue ${dbUser.name}`,
+      message: `Bienvenue ${dbUser.name}`,
       data: user,
-      token, // Retourne le token dans la réponse
+      token,
     });
   } catch (error: any) {
     return NextResponse.json(
